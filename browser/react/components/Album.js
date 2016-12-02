a31120b01c47d01b512f998b8ea37a9a8f9eda8b
Surabhi Nigam <surabhi@Surabhis-MacBook-Pro.local>
2016-12-02 14:46:11 -0500

    child props

--- browser/react/components/Album.js
+++ browser/react/components/Album.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Songs from '../components/Songs';
+import initialState from '../initialState';
 
-const Album = (props) => {
+export default class Album extends Component {
+constructor (props) {
+    super(props);
+  this.album = this.props.album;
+  this.currentSong = this.props.currentSong;
+  this.isPlaying = this.props.isPlaying;
+  this.toggleOne = this.props.toggleOne;
 
-  const album = props.album;
-  const currentSong = props.currentSong;
-  const isPlaying = props.isPlaying;
-  const toggleOne = props.toggleOne;
+  this.state = initialState;
+}
 
+  componentDidMount () {
+  const albumId = this.props.routeParams.albumId;
+  const selectAlbum = this.props.selectAlbum;
+
+  this.setState({
+      album: selectAlbum(albumId)
+    });
+}
+
+
+render(){
   return (
     <div className="album">
       <div>
-        <h3>{ album.name }</h3>
-        <img src={ album.imageUrl } className="img-thumbnail" />
+        <h3>{ this.state.album.name }</h3>
+        <img src={ this.state.album.imageUrl } className="img-thumbnail" />
       </div>
       <Songs
-        songs={album.songs}
-        currentSong={currentSong}
-        isPlaying={isPlaying}
-        toggleOne={toggleOne} />
+        songs={this.state.album.songs}
+        currentSong={this.state.currentSong}
+        isPlaying={this.state.isPlaying}
+        toggleOne={this.state.toggleOne} />
     </div>
   );
 }
 
-export default Album;
+}
--- browser/react/index.js
+++ browser/react/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './containers/AppContainer';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, hashHistory, IndexRedirect } from 'react-router';
 import Albums from './components/Albums.js';
 import Album from './components/Album.js';
-import IndexRedirect from 'react-router';
+//import IndexRedirect from 'react-router';
 
 
 ReactDOM.render(
@@ -12,7 +12,7 @@ ReactDOM.render(
   <Route path='/' component={AppContainer}>
   	<IndexRedirect to="/albums" />
     <Route path='/albums' component={Albums} /> 
-    <Route path='/album' component={Album} /> 
+    <Route path="albums/:albumId" component={Album} />
   </Route>
 </Router>,
 
