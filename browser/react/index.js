0467f11ab78b3cb96ef11ab4bda1af6c21c7e28c
Surabhi Nigam <surabhi@Surabhis-MacBook-Pro.local>
2016-12-02 12:34:08 -0500

    indexredirect

--- browser/react/index.js
+++ browser/react/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './containers/AppContainer';
 import { Router, Route, hashHistory } from 'react-router';
+import Albums from './components/Albums.js';
+import Album from './components/Album.js';
+import IndexRedirect from 'react-router';
 
 
 ReactDOM.render(
   <Router history={hashHistory}>
   <Route path='/' component={AppContainer}>
+  	<IndexRedirect to="/albums" />
     <Route path='/albums' component={Albums} /> 
+    <Route path='/album' component={Album} /> 
   </Route>
 </Router>,
 
